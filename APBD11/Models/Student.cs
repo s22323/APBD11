d4603de27fba9147b8e85615793a3c388d560725
Marta Peska <s22323@pjwstk.edu.pl>
2023-01-25 23:05:29 +0100

    Dodaj pliki projektów.

---
+++ APBD11/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace APBD11.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string AvatarUrl { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Birthdate { get; set; }
+        public string Studies { get; set; }
+    }
+}
